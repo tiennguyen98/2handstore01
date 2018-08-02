f5fa42562ceb2552479227a324697828b4e5cf6a
Tien Nguyen <nguyentien1098@gmail.com>
2018-08-02 20:30:23 +0700

    Add css/js admin template

---
+++ resources/lang/vi/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'failed' => 'Tài khoản hoặc mật khẩu không đúng.',
+    'throttle' => 'Bạn đã đăng nhập quá số nhiều lần. Vui lòng thử lại sau :seconds giây',
+    'login' => 'Đăng nhập',
+    'email' => 'Địa chỉ Email',
+    'password' => 'Mật khẩu',
+    'remember' => 'Ghi nhớ',
+    'forgot' => 'Quên mật khẩu',
+
+];
---
+++ resources/views/admin/assets/css.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/css/normalize.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/css/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/css/flag-icon.min.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/css/cs-skin-elastic.css') }}">
+<link rel="stylesheet" href="{{ asset('bower_components/sufee-admin-dashboard/assets/scss/style.css') }}">
+<link href='{{ asset('fonts/OpenSan.css') }}' rel='stylesheet' type='text/css'>
